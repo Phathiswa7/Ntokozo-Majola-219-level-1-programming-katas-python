5e1554ec0c2dd6570b763b29c76efda7ec99edb4
Phathiswa7 <159517280+Phathiswa7@users.noreply.github.com>
2024-09-09 15:24:29 +0200

    Update task4.py

--- task4.py
+++ task4.py
@@ -1,4 +1,4 @@
 def square(square_size):
  
- for iteration_counter in range(square_size):
-  print('#'* square_size)
+    for iteration_counter in range(square_size):
+        print('#'* square_size)
