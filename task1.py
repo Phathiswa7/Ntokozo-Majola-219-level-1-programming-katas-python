a8415e9527d814b139a95e53e7a00d9ab7f2cd91
Phathiswa7 <159517280+Phathiswa7@users.noreply.github.com>
2024-09-09 15:17:54 +0200

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,8 +1,7 @@
 def multiples(multiples_sum_value):
 
- sum = 0
- for counter in range(1,multiples_sum_value):
-  if counter % 3 == 0 or counter % 5 == 0 :
-   sum += counter
- 
- print(sum)  
+				sum = 0
+    for counter in range(1,multiples_sum_value):
+    				if counter % 3 == 0 or counter % 5 == 0 :
+        				sum += counter
+    print(sum)  
