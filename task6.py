0af6224bb19e07db2708ca619936599fcc33cb00
Phathiswa7 <159517280+Phathiswa7@users.noreply.github.com>
2024-09-09 15:28:02 +0200

    Update task6.py

--- task6.py
+++ task6.py
@@ -1,7 +1,6 @@
 def longest(words):
 
-  longest_word = max(len(word) for word in words)
-
-  for word in words:
-    if len(word) == longest_word:
-      print(word)
+    longest_word = max(len(word) for word in words)
+    for word in words:
+        if len(word) == longest_word:
+            print(word)
