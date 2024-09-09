8883f19a03af8ea6b8a089e2336497449b04b800
Phathiswa7 <159517280+Phathiswa7@users.noreply.github.com>
2024-09-09 15:23:29 +0200

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,3 +1,3 @@
 def is_sixty_five(number1,number2):
  
- return (number1 + number2 == 65) or (number1 == 65) or (number2 == 65)
+    return (number1 + number2 == 65) or (number1 == 65) or (number2 == 65)
